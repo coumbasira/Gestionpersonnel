6ea7f61123d1463395e57a4a59a7898ec501476a
coumba diarra <coumba.dev@gmail.com>
2023-08-17 11:16:47 +0000

    ajout d'utilisateur

--- conge/views.py
+++ conge/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from .forms import SignUpForm
 
 
 def liste(request):
@@ -27,3 +28,20 @@ def logout_user(request):
     logout(request)
     messages.success(request, " vous avez été déconnecté... ")
     return redirect('liste')
+
+def register_user(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            #s'authentifier et se connecter
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, "inscription valider bienvenue!")
+            return redirect('liste')
+    else:
+        form = SignUpForm()
+        return render(request, 'register.html', {'form':form})
+    return render(request, 'register.html', {'form':form})
